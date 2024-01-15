9a31a2bb35ed197c978aadd449dc393c4397864e
Ida Darmochwał <ida.mdart@gmail.com>
2024-01-15 16:18:53 +0100

    Init project

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+def get_current_date():
+  return {'curr_year': str(datetime.now().year)}
+
+
+app.context_processor(get_current_date)
+
+
+@app.route("/")
+def home():
+  return render_template("home.html")
+
+
+@app.route("/about/")
+def about():
+  return render_template("about.html")
+
+
+@app.route("/api/data")
+def get_data():
+  return app.send_static_file("data.json")
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
